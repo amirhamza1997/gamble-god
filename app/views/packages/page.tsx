a4cde231318c8024978eeca7c4217d41c443ae74
amirhamza1997 <amirhamza9464@gmail.com>
2023-07-31 05:32:48 -0700

    worked on packages description

--- app/views/packages/page.tsx
+++ app/views/packages/page.tsx
@@ -15,7 +15,7 @@ export default function New() {
           <div className='w-1/2'>
             <h3 className='text-[#D6AA28] pb-1'>WELCOME TO GAMBLE GOD, SCOTT lENNINGTON!</h3>
             <h1 className='font-bold text-4xl text-white'>Select your Player Package</h1>
-            <p className='text-white'>When selecting this package you have to be willing to bet $25 a unit per play to properly follow our system and maximize the 
+            <p className='text-white mt-4'>When selecting this package you have to be willing to bet $25 a unit per play to properly follow our system and maximize the 
               profits. You can expect to wager between 20-35 units a day with an average of 25 units per day.<br/><br/> This translates to $500 - $875 per 
               day with an average of $625 per day. With this package you can expect to win 3- 15 units a day which translates to $75 - $375 a 
               day in profit.<br/><br/> Weekly you can expect to win 45 - 60 units which translates to $1125 - $1500 in profits. These are projections 
@@ -23,7 +23,7 @@ export default function New() {
           </div>
           <div className='w-1/2 flex flex-col	gap-2'>
             <div className=' h-max bg-[#3D0069] relative'>
-              <div className='border-2 border-[#8A00CC] rounded-md	 p-6 text-white'>
+              <div className='border-2 border-[#8A00CC] rounded-md	 p-3 text-white'>
                 <div className='flex justify-between'>
                   <h3 className='text-xl pb-2'>Gold Package</h3>
                   <span className='text-[#D6AA28]'>$25 Unit Players</span>
@@ -35,7 +35,7 @@ export default function New() {
               </div>
             </div>
             <div className=' h-max bg-[#3D0069] relative'>
-              <div className='border-2 border-[#8A00CC] rounded-md	 p-6 text-white'>
+              <div className='border-2 border-[#8A00CC] rounded-md	 p-3 text-white'>
                 <div className='flex justify-between'>
                   <h3 className='text-xl pb-2'>Gold Package</h3>
                   <span className='text-[#D6AA28]'>$25 Unit Players</span>
@@ -47,7 +47,7 @@ export default function New() {
               </div>
             </div>
             <div className=' h-max bg-[#3D0069] relative'>
-              <div className='border-2 border-[#8A00CC] rounded-md	 p-6 text-white'>
+              <div className='border-2 border-[#8A00CC] rounded-md	 p-3 text-white'>
                 <div className='flex justify-between'>
                   <h3 className='text-xl	pb-2'>Gold Package</h3>
                   <span className='text-[#D6AA28]'>$25 Unit Players</span>
@@ -58,9 +58,146 @@ export default function New() {
                 </div>
               </div>
             </div>
+            <button className='relative bg-[#D6AA28] rounded-md p-1 mt-3'>Continue</button>
           </div>
         </div>
       </Container>
+      <div className='bg-[#3D0069] relative mt-16 text-white'>
+        <Container>
+          <h2 className='text-2xl	font-semibold	py-12 capitalize'>Additional features and services</h2>
+          <div className='flex gap-4 pb-8'>
+            <div className='border-2 border-[#8A00CC] rounded-md flex-1'>
+              <h3 className='text-center p-2 bg-[#8A00CC]'>GOLD PACKAGE</h3>
+              <div className='flex gap-2 mt-4'>
+                <div>
+                  <Image src={'/images/Vector.png'} width={50} height={50} alt={'Vector'}/>
+                </div>
+                
+                <div className='text-base	'>
+                  <span>One Day Gamble God Package</span><br />
+                  <span className='bg-[#D6AA28] rounded-md'>$25 + Fees</span>
+                  <p className='text-sm	mt-2'> With this package you will receive the plays for a one day period.</p>
+                </div>
+              </div>
+              <div className='flex gap-2 mt-8'>
+                <div>
+                  <img src="/images/Vector.png" width={150} height={150} alt="" />
+                </div>
+                
+                <div className='text-base	'>
+                  <span>7 Day - Weekly Gamble God Package</span><br />
+                  <span className='bg-[#D6AA28] rounded-md'>$140 + Fee (20% discount)</span><br />
+                  <span>**RECOMMENDED**</span><br /><br />
+                  <span className='bg-[#D6AA28] rounded-md'>100 % MONEY BACK GUARANTEE</span>
+                  <p className='text-sm	mt-2'>With this package you will receive all the plays released for a 7 day period. 
+                  If at the end of the 7 day period the package doesn’t result in a profit (Positive units), 
+                  the pack age price will be refunded to you.</p>
+                </div>
+              </div>
+              <div className='flex gap-2 mt-8'>
+                <div>
+                  <img src="/images/Vector.png" width={150} height={150} alt="" />
+                </div>
+                
+                <div className='text-base	pb-8'>
+                  <span>30 Day - Weekly Gamble God Package</span><br />
+                  <span className='bg-[#D6AA28] rounded-md'>$525 + Fee (30% discount)</span><br /><br />
+                  {/* <span>**RECOMMENDED**</span><br /><br /> */}
+                  <span className='bg-[#D6AA28] rounded-md'>100 % MONEY BACK GUARANTEE</span>
+                  <p className='text-sm	mt-2'>With this package you will receive all the plays released for a 7 day period. If at the end of the
+                   7 day period the package doesn’t result in a profit (Positive units), the pack age price will be refunded to you.</p>
+                </div>
+              </div>
+            </div>
+            <div className='border-2 border-[#8A00CC] rounded-md flex-1'>
+              <h3 className='text-center p-2 bg-[#D6AA28]'>PLATINUM PACKAGE</h3>
+              <div className='flex gap-2 mt-4'>
+                <div>
+                  <Image src={'/images/Vector.png'} width={50} height={50} alt={'Vector'}/>
+                </div>
+                
+                <div className='text-base	'>
+                  <span>One Day Gamble God Package</span><br />
+                  <span className='bg-[#D6AA28] rounded-md'>$50 + Fees</span>
+                  <p className='text-sm	mt-2'> With this package you will receive the plays for a one day period.</p>
+                </div>
+              </div>
+              <div className='flex gap-2 mt-8'>
+                <div>
+                  <img src="/images/Vector.png" width={150} height={150} alt="" />
+                </div>
+                
+                <div className='text-base	'>
+                  <span>7 Day - Weekly Gamble God Package</span><br />
+                  <span className='bg-[#D6AA28] rounded-md'>$280 + Fee (20% discount)</span><br />
+                  <span>**RECOMMENDED**</span><br /><br />
+                  <span className='bg-[#D6AA28] rounded-md'>100 % MONEY BACK GUARANTEE</span>
+                  <p className='text-sm	mt-2'>With this package you will receive all the plays released for a 7 day period. 
+                  If at the end of the 7 day period the package doesn’t result in a profit (Positive units), 
+                  the pack age price will be refunded to you.</p>
+                </div>
+              </div>
+              <div className='flex gap-2 mt-8'>
+                <div>
+                  <img src="/images/Vector.png" width={150} height={150} alt="" />
+                </div>
+                
+                <div className='text-base	pb-8'>
+                  <span>30 Day - Weekly Gamble God Package</span><br />
+                  <span className='bg-[#D6AA28] rounded-md'>$1050 + Fee (30% discount)</span><br /><br />
+                  {/* <span>**RECOMMENDED**</span><br /><br /> */}
+                  <span className='bg-[#D6AA28] rounded-md'>100 % MONEY BACK GUARANTEE</span>
+                  <p className='text-sm	mt-2'>With this package you will receive all the plays released for a 7 day period. If at the end of the
+                   7 day period the package doesn’t result in a profit (Positive units), the pack age price will be refunded to you.</p>
+                </div>
+              </div>
+            </div>
+            <div className='border-2 border-[#8A00CC] rounded-md flex-1'>
+              <h3 className='text-center p-2 bg-[#8A00CC]'>GOLD PACKAGE</h3>
+              <div className='flex gap-2 mt-4'>
+                <div>
+                  <Image src={'/images/Vector.png'} width={50} height={50} alt={'Vector'}/>
+                </div>
+                
+                <div className='text-base	'>
+                  <span>One Day Gamble God Package</span>
+                  <span className='bg-[#D6AA28] rounded-md'>$100 + Fees</span>
+                  <p className='text-sm	mt-2'> With this package you will receive the plays for a one day period.</p>
+                </div>
+              </div>
+              <div className='flex gap-2 mt-8'>
+                <div>
+                  <img src="/images/Vector.png" width={150} height={150} alt="" />
+                </div>
+                
+                <div className='text-base	'>
+                  <span>7 Day - Weekly Gamble God Package</span><br />
+                  <span className='bg-[#D6AA28] rounded-md'>$560 + Fee (20% discount)</span><br />
+                  <span>**RECOMMENDED**</span><br /><br />
+                  <span className='bg-[#D6AA28] rounded-md'>100 % MONEY BACK GUARANTEE</span>
+                  <p className='text-sm	mt-2'>With this package you will receive all the plays released for a 7 day period. 
+                  If at the end of the 7 day period the package doesn’t result in a profit (Positive units), 
+                  the pack age price will be refunded to you.</p>
+                </div>
+              </div>
+              <div className='flex gap-2 mt-8'>
+                <div>
+                  <img src="/images/Vector.png" width={150} height={150} alt="" />
+                </div>
+                
+                <div className='text-base	pb-8'>
+                  <span>30 Day - Weekly Gamble God Package</span><br />
+                  <span className='bg-[#D6AA28] rounded-md'>$2100 + Fee (30% discount)</span><br /><br />
+                  {/* <span>**RECOMMENDED**</span><br /><br /> */}
+                  <span className='bg-[#D6AA28] rounded-md'>100 % MONEY BACK GUARANTEE</span>
+                  <p className='text-sm	mt-2'>With this package you will receive all the plays released for a 7 day period. If at the end of the
+                   7 day period the package doesn’t result in a profit (Positive units), the pack age price will be refunded to you.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Container>
+      </div>
     </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,9 @@ module.exports = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-primary': 'linear-gradient(90deg, #D6AA28 0.93%, #FC9500 100%)'
+          
       }
     },
     screens: {
